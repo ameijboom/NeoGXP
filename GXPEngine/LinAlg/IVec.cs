c7d81be15106b8a13526c27ed9cf78da1fb7f083
LordPINE <middelhylke@gmail.com>
2022-04-25 15:17:34 +0200

    Create IVec interface

--- GXPEngine/LinAlg/IVec.cs
+++ GXPEngine/LinAlg/IVec.cs
@@ -2,6 +2,13 @@ namespace NeoGXP.GXPEngine.LinAlg
 {
     public interface IVec
     {
-         public IVec Multiply(IMatrix matrix);
+        public float GetElement();
+        public float Magnitude();
+        public float MagnitudeSquared();
+        public IVec Scale(float scalar);
+        public IVec Add(IVec vector);
+        public float Dot(IVec vector);
+        public float Cross(IVec vector);
+        public IVec Multiply(IMatrix matrix);
     }
 }
