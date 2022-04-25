cdfa0c40892c6198158c07e9414956f01959c041
Patrycioss <72610925+Patrycioss@users.noreply.github.com>
2022-04-25 21:32:58 +0200

    Everything should now actually work :D

--- MyGame.cs
+++ MyGame.cs
@@ -2,22 +2,20 @@
 
 public class MyGame : Game
 {
-    private EasyDraw canvas;
+    private Player player;
     
     
     private MyGame() : base(800, 600, false)
     {
-        canvas = new EasyDraw(800, 600);
-        AddChild(canvas);
-        
-        canvas.Ellipse(width/2.0f,height/2.0f,200,200);
+        player = new Player();
+        AddChild(player);
+
     }
 
     private void Update()
     {
         
     }
-
     private static void Main()
     {
         new MyGame().Start();
---
+++ Player.cs
@@ -0,0 +1,11 @@
+﻿using GXPEngine.Core;
+
+namespace GXPEngine;
+
+public class Player : Sprite
+{
+    public Player() : base(new Texture2D("square.png"))
+    {
+        
+    }
+}
