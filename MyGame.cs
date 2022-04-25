91f74c82131ef79a714a157ffc0c9f917b827951
Patrycioss <72610925+Patrycioss@users.noreply.github.com>
2022-04-25 15:57:15 +0200

    Added MyGame.cs

---
+++ MyGame.cs
@@ -0,0 +1,25 @@
+﻿namespace GXPEngine;
+
+public class MyGame : Game
+{
+    private EasyDraw canvas;
+    
+    
+    private MyGame() : base(800, 600, false)
+    {
+        canvas = new EasyDraw(800, 600);
+        AddChild(canvas);
+        
+        canvas.Ellipse(width/2.0f,height/2.0f,200,200);
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private static void Main()
+    {
+        new MyGame().Start();
+    }
+}
